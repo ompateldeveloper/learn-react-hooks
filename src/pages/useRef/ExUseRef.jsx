ad5467c6b7dc067f070b1adbab32e036f23ed01e
ompateldeveloper <ompatel.developer@gmail.com>
2024-02-27 18:07:27 +0530

    fixed in useref

--- src/pages/useRef/ExUseRef.jsx
+++ src/pages/useRef/ExUseRef.jsx
@@ -4,8 +4,6 @@ export default function ExUseRef() {
     const containerRef = useRef()
     useEffect(()=>{
         containerRef.current.classList.add('bg-red-500')
-        
-        containerRef.current.appendChild()
         return ()=>{
             if(containerRef.current){ 
                 // containerRef.current.classList.remove('bg-red-500')  
